cb0b6d2188ba4f7a07c10f924de3e94bcc04bed1
Sayantan <Sayantanchowdhury615@gmail.com>
2022-07-19 14:17:05 +0530

    Ready for upload

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+//TO RENDER THE CSS AND IMAGES
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        // status: "unsubscribed",
+        merge_fields: {
+          FNAME: fname,
+          LNAME: lname,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us12.api.mailchimp.com/3.0/lists/6f904d797e";
+
+  const option = {
+    method: "post",
+    auth: "sayantan615:6ca2b57063576166cc07cea616b74fa0-us12",
+  };
+
+  const request = https.request(url, option, (response) => {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/success", (req, res) => {
+  res.redirect("/");
+});
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000);
